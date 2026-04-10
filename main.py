ecc92602d86d52e0e4872df25986bf2f0c209783
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-04-10 21:39:03 +0530

    Load data in main function and print results

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from src.config import DATA_PATH
+from src.data_loader import load_data
 
+
+def main():
+    df = load_data(DATA_PATH)
+
+    print("Data Loaded Successfully")
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
